8b55fbce9256c7e32cbe08cf5ae6cf7031004b64
Michael Lai <themichaellai@gmail.com>
2014-09-13 10:10:14 -0400

    Added call function

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
 var app = require('express')();
 var bodyParser = require('body-parser');
+var twilioClient = require('twilio')(
+  process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
+var twilioNumber = process.env.TWILIO_PHONE_NUMBER;
+var url = require('url');
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
+
+var placeCall = function(phoneNumber, tonesSoFar) {
+  client.calls.create({
+      from: twilioNumber,
+      to: phoneNumber, //  automated number
+      url: url.format({
+        host: 'http://guarded-retreat-7641.herokuapp.com/',
+        query: {
+          tonesSoFar: tones
+        }
+      }),
+      record: "true",
+      method: "GET",
+      fallbackMethod: "GET",
+      statusCallbackMethod: "GET",
+  }, function(err, call) {
+      if (err) {
+          console.log(err);
+          throw err;
+      }
+      console.log("Placing call...");
+      console.log("call info:");
+      console.log(call);
+  });
+};
 
 // queries: tonesSoFar, number
 // TODO: parse the transcribed text into an object and update object in db
@@ -25,7 +54,6 @@ app.get('/scrape', function(req, res) {
 app.get('/retrieve/:number', function(req, res) {
 	res.send("TODO: return object from db");
 });
-
 
 
 
