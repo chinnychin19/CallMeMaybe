d03dd200ace6b18507e70cc6f4eccf60e0547fe3
Chinmay Patwardhan <chinnychin19@gmail.com>
2014-09-13 15:10:06 -0400

    improved regex parsing

--- transcriptionParser.js
+++ transcriptionParser.js
@@ -1,10 +1,50 @@
 var fs = require('fs');
 
+var keyMap = {
+    1:1,
+    2:2,
+    3:3,
+    4:4,
+    5:5,
+    6:6,
+    7:7,
+    8:8,
+    1:9,
+    0:0,
+    "one":1,
+    "two":2,
+    "three":3,
+    "four":4,
+    "five":5,
+    "six":6,
+    "seven":7,
+    "eight":8,
+    "nine":9,
+    "zero":0,
+    "star": '*',
+    "*":"*",
+    "asterisk":"*",
+    "#":"#",
+    "pound": "#"
+};
 
 exports.parse = function parse(text) {
     var obj = {};
+    
+    // Represents numbers 0-9 spelt out
+    var re_word_num = new RegExp(/zero|one|two|three|four|five|six|seven|eight|nine|ten/);
+    // 0-9
+    var re_num_num = new RegExp(/[0-9]/);
+    //Either of the above
+    var re_num = new RegExp(re_word_num.source + "|" + re_num_num.source);
+    // press and the spce
+    var re_press = new RegExp(/press\s/);
+    // the # key
+    var re_pound = new RegExp(/the\s(\#)\skey/);
+    // Combine it all together and hope for the best
+    var re = new RegExp(re_press.source + "((" + re_num.source + ")|" + re_pound.source + ")");
 
-    var re = /press\s(([0-9])|the\s(\#)\skey)/;
+    //var re = /press\s(([0-9])|the\s(\#)\skey)/;
     var result = text.match(re);
     var end = result["index"] + result[0].length;
     var prefix = text.substring(0, end);
@@ -15,7 +55,7 @@ exports.parse = function parse(text) {
     var first = text.substring(start, end);
     text = text.substring(end).trim();
     var key = result[2] || result[3];
-    obj[key] = first;
+    obj[keyMap[key]] = [first, null];
 
     while (text.length > 0) {
         if (text.indexOf('press') < 0) break;
@@ -26,7 +66,7 @@ exports.parse = function parse(text) {
         if(!!obj[key]) {
             return obj;
         }
-        obj[key] = [result[0], null];
+        obj[keyMap[key]] = [result[0], null];
 
         text = text.substring(result['index']+result[0].length).trim();
     }
@@ -34,20 +74,20 @@ exports.parse = function parse(text) {
     return obj;
 }
 
-// var text = "(Wendy?) and where "+
-// "if you know your party's 5 digit extension number or access the company directory by last name please press the # key "+
-// "for licensing services and accounts press 1 "+
-// "for sales press 2 "+
-// "for contract renewals press 3 "+
-// "for technical support press 4 "+
-// "for educational and consulting services press 5 "+
-// "for Richard to pay and expense reimbursements press 6 "+
-// "for accounts receivable press 7 to talk to our customer care.";
+var text = "(Wendy?) and where "+
+"if you know your party's 5 digit extension number or access the company directory by last name please press the # key "+
+"for licensing services and accounts press 1 "+
+"for sales press 2 "+
+"for contract renewals press 3 "+
+"for technical support press 4 "+
+"for educational and consulting services press 5 "+
+"for Richard to pay and expense reimbursements press 6 "+
+"for accounts receivable press 7 to talk to our customer care.";
 
 
-// var text = "Hello, welcome to out if you would like to speak to sales press 1 if you are premium support customer press 2 for other technical support questions we're always available at help at (toyota.com?) for general information about Leo press 3 to hear these options again press 4 hello welcome to Leo out if you would like to speak to sales press 1 if you are premium support customer press 2 for other technical support questions we're always available at help at (toyota.com?) for general information about the Leo press 3 to hear these options again press 4 hello welcome to out if you would like to speak to sales press 1 if you are premium support customer press 2 for other technical support questions we're always available help out Leo.com for general information about."
+var text = "You for calling me where if you know your party's 5 digit extension number or access the company directory by last name please press the # key for licensing services and accounts press 1 for sales press 2 for contract renewals press 3 for technical support press 4 for educational and consulting services press 5 for Richard to pay and expense reimbursement press 6 for accounts receivable press 7 to talk to our customer care representative please press 0 if you would like to repeat these options press nine we're continue to stay on the line."
 
-// exports.parse(text);
+console.log(exports.parse(text));
 
 
 
